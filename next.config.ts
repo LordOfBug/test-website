9f81fca0522378147c37f73f8987987240f0c904
nchen <nchen@somewhere>
2026-04-22 14:00:20 +0800

    fix: logo layout and force replace burros.png asset

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
+  images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
